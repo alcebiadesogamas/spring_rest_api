1ecc28dedb01b93ed17c3c884ed7596d9aef6c97
alcebiades <alcebiades.29@gmail.com>
2024-01-03 15:15:12 -0300

    first commit

---
+++ src/test/java/alcebiades/spring_rest_api/AppTests.java
@@ -0,0 +1,13 @@
+package alcebiades.spring_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
